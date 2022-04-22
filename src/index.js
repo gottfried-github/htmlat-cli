4539e4d752aec40d3049ae10618464d4a2dad0bf
spti <tobetodee@gmail.com>
2022-04-22 16:00:56 +0300

    use convertFile from the updated htmlat-nodejs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 #! /usr/bin/env node
 
-import {convert} from 'htmlat-nodejs'
+import {convertFile} from 'htmlat-nodejs'
 
 function main() {
     const io = process.argv.slice(2, 4), rest = process.argv.slice(4)
     const raw = rest.includes('--raw'), wrap = rest.includes('--wrap')
 
-    convert(...io, {raw, wrap})
+    convertFile(...io, {raw, wrap})
 }
 
 main()
