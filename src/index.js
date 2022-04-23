f72a66d00d124e5c54478a9b2d7ef951776275a7
spti <tobetodee@gmail.com>
2022-04-23 15:17:13 +0300

    mapArgs: fix option matching

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ function mapArgs(args) {
     // const io = process.argv.slice(2, 4), rest = process.argv.slice(4)
 
     const o = args.reduce((opts, arg, i) => {
-        if (arg.includes('--')) {
+        if (arg.match('^--')) {
             if (0 === Object.keys(opts).length) {
                 opts.prefix = args.slice(0, i)
             }
 
-            opts[arg] = -1 === args.slice(i+1).findIndex(v => v.includes('--'))
+            opts[arg] = -1 === args.slice(i+1).findIndex(v => v.match('^--'))
                 ? args.slice(i+1)
-                : args.slice(i+1, i+args.slice(i+1).findIndex(v => v.includes('--'))+1)
+                : args.slice(i+1, i+args.slice(i+1).findIndex(v => v.match('^--'))+1)
 
             return opts
         }; return opts
